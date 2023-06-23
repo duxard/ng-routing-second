65e4abc5a61b739f1af350fe6946701641228d07
duxard <duxard@gmail.com>
2023-06-23 15:09:21 +0300

    add example for a template with context (ng-container)

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -8,6 +8,12 @@ import {
   QueryList, TemplateRef, ViewChild, ViewContainerRef
 } from '@angular/core';
 
+interface TemplateContextInterface {
+  name: string;
+  age: number;
+  admin: boolean;
+}
+
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
@@ -15,7 +21,14 @@ import {
 })
 export class TestComponent implements AfterViewInit {
   @ViewChild('ngContainer', {read: ViewContainerRef}) ngContainer!: ViewContainerRef;
-  @Input() template!: TemplateRef<any>;
+  @Input() template!: TemplateRef<void>;
+  @Input() templateWithContext!: TemplateRef<TemplateContextInterface>;
+
+  contextForTemplate: TemplateContextInterface = {
+    name: 'john',
+    age: 100,
+    admin: true
+  };
 
   ngAfterViewInit(): void {
     this.ngContainer.createEmbeddedView(this.template);
