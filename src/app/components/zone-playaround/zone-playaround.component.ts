50060e7741d2b795a93c5b4f66033174d134234f
duxard <duxard@gmail.com>
2023-07-05 16:20:52 +0300

    Update

--- src/app/components/zone-playaround/zone-playaround.component.ts
+++ src/app/components/zone-playaround/zone-playaround.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, DoCheck, NgZone} from '@angular/core';
+import { Component, DoCheck, NgZone} from '@angular/core';
 import { of } from 'rxjs';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
@@ -6,15 +6,16 @@ const dummyApi = () => of(['one', 'two', 'three']);
 
 @UntilDestroy()
 @Component({
-  changeDetection:  ChangeDetectionStrategy.OnPush,
   selector: 'app-zone-playaround',
   styles: [],
   template: `
     <button (click)="onClick()">Play Zone</button>
+    <p>{{text}}</p>
   `
 })
 export class ZonePlayaroundComponent implements DoCheck {
   private doCheckCount = 0;
+  text = 'initial text';
 
   constructor(private zone: NgZone) { }
 
@@ -23,9 +24,21 @@ export class ZonePlayaroundComponent implements DoCheck {
   }
 
   onClick(): void {
-    console.clear();
-    dummyApi().pipe(untilDestroyed(this)).subscribe(data => {
-      console.log("Data received");
+    this.zone.runOutsideAngular(() => {
+      dummyApi().pipe(untilDestroyed(this)).subscribe(_ => {
+        console.log('onClick');
+        this.text = 'onClick'
+      });
+    });
+  }
+
+  ngOnInit(): void {
+    this.zone.runOutsideAngular(() => {
+      setTimeout(() => {
+        console.log('setTimeout');
+        this.text = 'setTimeout'
+      }, 1000);
     });
   }
 }
+
