4ac70f958765a99605c312ab71c948bd0af18c81
duxard <duxard@gmail.com>
2023-08-23 21:23:05 +0300

    Add a story for a button

--- stories/custom/TuiButton.stories.ts
+++ stories/custom/TuiButton.stories.ts
@@ -1,5 +1,6 @@
 import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
 import { TuiButtonModule } from './components/tui-button/tui-button.module';
+import { action } from '@storybook/addon-actions';
 
 const meta: Meta = {
   title: 'Example/TuiButton',
@@ -7,7 +8,35 @@ const meta: Meta = {
     moduleMetadata({
       imports: [TuiButtonModule]
     })
-  ]
+  ],
+  argTypes: {
+    icon: {
+      control: { type: 'select', defaultValue: 'alert' },
+      options: ['alert', 'pointer', 'arrow', 'default'],
+      defaultValue: 'alert'
+    },
+    variant: {
+      control: { type: 'radio' },
+      options: ['small', 'middle', 'large', 'extra-large'],
+
+    },
+    text: { type: 'string', defaultValue: 'Default text' }
+  },
+  args: {
+    icon: 'alert',
+    variant: 'small',
+    text: 'lorem ipsum'
+  },
+  parameters: {
+    backgrounds: {
+      values: [
+        { name: 'black', value: '#000' },
+        { name: 'dark', value: '#222' },
+        { name: 'light', value: '#ddd' },
+        { name: 'white', value: '#fff' },
+      ]
+    }
+  }
 };
 
 export default meta;
@@ -16,7 +45,28 @@ export const Basic: StoryObj = {
   render: (props) => ({
     props,
     template: `
-      <tui-button></tui-button>  
+      <tui-button
+        [icon]="icon"
+        [variant]="variant"
+        [text]="text"
+      ></tui-button>
+    `
+  })
+};
+
+export const WithClickHandler: StoryObj = {
+  render: (props) => ({
+    props: {
+      ...props,
+      onButtonClick: action('onButtonClick event')
+    },
+    template: `
+      <tui-button
+        [icon]="icon"
+        [variant]="variant"
+        [text]="text"
+        (onButtonClick)="onButtonClick($event)"
+      ></tui-button>
     `
   })
 };
--- stories/custom/components/tui-button/tui-button.component.ts
+++ stories/custom/components/tui-button/tui-button.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+
+type IconType = 'alert' | 'pointer' | 'arrow' | 'default';
+type VariantType = 'small' | 'middle' | 'large' | 'extra-large';
 
 @Component({
   selector: 'tui-button',
   template: `
-    <button>My button</button>
+    <button (click)="onClick()">My button</button>
+    <div>Inputs (props):</div>
+    <div>Icon: {{icon}}</div>
+    <div>Variant: {{variant}}</div>
+    <div>Text: {{text}}</div>
   `
 })
 export class TuiButtonComponent {
+  @Input() icon: IconType = 'default';
+  @Input() variant: VariantType = 'small';
+  @Input() text: string = '';
+  @Output() onButtonClick = new EventEmitter<string>();
+
+  onClick(): void {
+    this.onButtonClick.emit('clicked');
+  }
 }
